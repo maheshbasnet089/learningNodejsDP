dd946013d4bf4106481bfe010b5de2cb581888de
maheshbasnet089 <basnetmanish089@gmail.com>
2023-08-02 20:51:11 +0545

    day2

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express"); // require gareko express lai
+const app = express(); // tyo require gareko lai  call gareko
+
+// app vanney mathi ko variable chae throughout the project use hunchha
+
+// port no(room No) : 1300 - 650000, 1300 vanda tala chae internal system lay use garirahunchha
+
+app.listen(4000, function () {
+  console.log("Node js project started");
+});
