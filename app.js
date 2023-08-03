33def85c22c300a81664d4157f2b0ebdd47553d9
maheshbasnet089 <basnetmanish089@gmail.com>
2023-08-03 21:00:31 +0545

    learnt about get,req,res cycle,nodemon

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require("express"); // require gareko express lai
 const app = express(); // tyo require gareko lai  call gareko
-
 // app vanney mathi ko variable chae throughout the project use hunchha
+
+app.get("/", (req, res) => {
+  res.send("<h2>hello world </h2>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("I am from about page");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("I am from contact page");
+});
 
 // port no(room No) : 1300 - 650000, 1300 vanda tala chae internal system lay use garirahunchha
 
