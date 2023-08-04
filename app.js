7a1975f6db8022bfb0b22b956009f7276cb5f52a
maheshbasnet089 <basnetmanish089@gmail.com>
2023-08-04 20:56:49 +0545

    ejs,ssr,csr

--- app.js
+++ app.js
@@ -2,17 +2,23 @@ const express = require("express"); // require gareko express lai
 const app = express(); // tyo require gareko lai  call gareko
 // app vanney mathi ko variable chae throughout the project use hunchha
 
+// setting up ejs, telling nodejs to use ejs 
+app.set('view engine','ejs')
+
 app.get("/", (req, res) => {
-  res.send("<h2>hello world </h2>");
+  res.render('home',{name :"Mahesh"})
+  // res.render("home",{name:"Manish"}) passing value to home.ejs 
+  
 });
 
 app.get("/about", (req, res) => {
-  res.send("I am from about page");
+  res.render('about')
 });
 
 app.get("/contact", (req, res) => {
-  res.send("I am from contact page");
+  res.render('contact')
 });
+
 
 // port no(room No) : 1300 - 650000, 1300 vanda tala chae internal system lay use garirahunchha
 
